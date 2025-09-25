fd93066136b8792dbb4b40f2f9993d83c289aac9
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2025-09-25 15:59:32 -0400

    add denoising step to process_ihmt and set kernel size

--- run_proc_ihmt.sh
+++ run_proc_ihmt.sh
@@ -5,17 +5,20 @@
 # Make sure ANTS path is set in bash profile
 # edited lines 87 and 88 in 'process_ihMT.sh' to remove '-n' flag (older BASH version of the command?)
 # does denoising, unringing, motion correction, computes maps
+# set denoising kernel extent (multiplied) to =< total number of raw volumes (n=12)
 # run as sh run_proc_ihmt.sh subject session
 
 
 export input=/Volumes/G-DRIVE/preschool_bids
-export output=/Volumes/G-DRIVE/preschool_bids/derivatives/proc_ihmt
+export output=/Volumes/G-DRIVE/preschool_bids/derivatives/ihmt_proc2
 
 #gunzip ${input}/${1}/${2}/${1}_${2}_ihMT.nii.gz
 
 sh ihmt_proc/process_ihmt.sh -i ${input}/${1}/${2}/anat/${1}_${2}_ihMT.nii.gz -o ${output}/${1}/${2}/${1}_${2}_ \
     -c ihMT,ihMTR,MTRs,MTRd,ihMTRinv,MTRsinv,MTRdinv \
     -n 4 \
+    -d 1 \
+    -e 4,4,4 \
     -u 1 \
     -m 1 \
     -R 1 \
