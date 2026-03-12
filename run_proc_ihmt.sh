e3eefad3c9ecbf4ce8885cd99266ac8e0614d659
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2026-03-12 16:36:39 -0400

    add back denoising and omit N4BiasCorrect ahead of ihmt_proc due to includion in mc-moco call

--- run_proc_ihmt.sh
+++ run_proc_ihmt.sh
@@ -8,19 +8,19 @@
 # set denoising kernel extent (multiplied) to =< total number of raw volumes (n=12)
 # run as sh run_proc_ihmt.sh subject session
 # Updates 12 March 2026:
-### adjusted to run following ANTS N4 bias correction of raw ihMT data, eventually merge to run workflow in a single script
 ### to 'scrub' out a bad volume, remove the volume number from -S or -D in the process_ihmt.sh run command
 
-export output=/Volumes/G-DRIVE/preschool_bids/derivatives/ihmt_proc_test
+export input=/Volumes/G-DRIVE/preschool_bids/
+export output=/Volumes/G-DRIVE/preschool_bids/derivatives/ihmt_proc_test_scrubbing
 
 
 #gunzip ${input}/${1}/${2}/${1}_${2}_ihMT.nii.gz
 
-sh ihmt_proc/process_ihmt.sh -i ${output}/${1}/${2}/${1}_${2}_ihMT_biascorrected.nii.gz -o ${output}/${1}/${2}/${1}_${2}_ \
+sh ihmt_proc/process_ihmt.sh -i ${input}/${1}/${2}/${1}_${2}_ihMT.nii.gz -o ${output}/${1}/${2}/${1}_${2}_ \
     -c ihMT,ihMTR,MTRs,MTRd,ihMTRinv,MTRsinv,MTRdinv \
     -n 4 \
-    -d 1 \
-    -e 3,3,3 \
+    -d y \
+    -e 4,4,4 \
     -u 1 \
     -m 1 \
     -R 1 \
